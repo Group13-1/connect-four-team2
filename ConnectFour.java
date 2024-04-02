83a3fb01ea597921c6dd9c53e9736e5a47caf8d6
NG CHUN YIN 22027058D <95467201+chunyinng078@users.noreply.github.com>
2024-04-03 01:07:00 +0800

    Update ConnectFour.java
    Define basic variables

--- ConnectFour.java
+++ ConnectFour.java
@@ -3,5 +3,12 @@ import java.util.Scanner;
 
 public class ConnectFour {
 
+    public static final int ROWS = 6;
+    public static final int COLUMNS = 7;
+    private static final char EMPTY_SLOT = '.';
+    private static final char[] PLAYERS = {'X', 'O'};
+    private char[][] board;
+    private int moveCount;
+
 
 }
